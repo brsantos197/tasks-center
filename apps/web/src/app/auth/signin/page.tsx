2c6164ea7d874f2ff8f25295f0ee36329d3c29c9
Wagner Vital <wagnervitaldo@gmail.com>
2024-05-11 20:33:36 -0300

    feat: create new Component
    criado novo componente auth-form

--- apps/web/src/app/auth/signin/page.tsx
+++ apps/web/src/app/auth/signin/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@tasks-center/ui/components/ui/button";
-import { Input } from "@tasks-center/ui/components/ui/input";
-import { Label } from "@tasks-center/ui/components/ui/label";
 import Image from "next/image";
 import Link from "next/link";
 
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+import { AuthForm } from "../../components/auth-form";
+
 export default async function SignIn() {
   return (
     <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
@@ -17,41 +14,7 @@ export default async function SignIn() {
               Enter your email below to login to your account
             </p>
           </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full gap-2">
-              <FcGoogle />
-              Login with Google
-            </Button>
-
-            <Button variant="outline" className="w-full gap-2">
-              <FaGithub />
-              Login with Github
-            </Button>
-          </div>
+          <AuthForm />
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
             <Link href="#" className="underline">
---
+++ apps/web/src/app/components/auth-form.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Button } from "@tasks-center/ui/components/ui/button";
+import { Input } from "@tasks-center/ui/components/ui/input";
+import { Label } from "@tasks-center/ui/components/ui/label";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
+export const AuthForm = () => {
+  return (
+    <div className="grid gap-4">
+      <div className="grid gap-2">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" type="email" placeholder="m@example.com" required />
+      </div>
+      <div className="grid gap-2">
+        <div className="flex items-center">
+          <Label htmlFor="password">Password</Label>
+          <Link
+            href="/forgot-password"
+            className="ml-auto inline-block text-sm underline"
+          >
+            Forgot your password?
+          </Link>
+        </div>
+        <Input id="password" type="password" required />
+      </div>
+      <Button type="submit" className="w-full">
+        Login
+      </Button>
+      <Button variant="outline" className="w-full gap-2">
+        <FcGoogle />
+        Login with Google
+      </Button>
+
+      <Button variant="outline" className="w-full gap-2">
+        <FaGithub />
+        Login with Github
+      </Button>
+    </div>
+  );
+};
